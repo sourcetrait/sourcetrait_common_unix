155cead1551c545c7ccde9bd5ec439acfede46bd
roylaurie <roy.laurie.pub@sourcetrait.com>
2025-12-18 07:57:27 -0800

    wip unix_fs

--- crates/sourcetrait_crossplat_unix/src/unix_fs/copy_preserved.rs
+++ crates/sourcetrait_crossplat_unix/src/unix_fs/copy_preserved.rs
@@ -1,131 +1,252 @@
 use crate::*;
 
+#[derive(Debug, Clone, Default, PartialEq, Eq, Hash)]
+pub struct CopyPreservedOptions {
+    pub lossy_attributes: bool,
+}
+
+/// Describes an IO error that was either cleanly recovered from or not.
+/// A clean recovery removed any file that was created.
+/// A dirty recovery failed to remove any file that was created. 
+#[derive(Debug, snafu::Snafu)]
+pub enum CopyPreservedError {
+    Clean {
+        src: PathBuf,
+        dst: PathBuf,
+        source: io::Error
+    },
+    Dirty {
+        src: PathBuf,
+        dst: PathBuf,
+        source: io::Error,
+        recover_source: io::Error,
+    }
+}
+
+impl CopyPreservedError {
+    fn clean<P1: AsRef<Path>, P2: AsRef<Path>>(src: P1, dst: P2, source: io::Error) -> Self {
+        Self::Clean { src: src.as_ref().into(), dst: dst.as_ref().into(), source }
+    }
+}
+
+fn recover<P1: AsRef<Path>, P2: AsRef<Path>>(src: P1, dst: P2, source: io::Error) -> CopyPreservedError {
+    let src = src.as_ref().to_path_buf();
+    let dst = dst.as_ref().to_path_buf();
+    
+    if fs::exists(&dst).is_ok_and(|exists| !exists) {
+        return CopyPreservedError::Clean { src, dst, source };
+    }
+    
+    match fs::remove_file(&dst) {
+        Ok(_) => CopyPreservedError::Clean { src, dst, source },
+        Err(recover_source) => CopyPreservedError::Dirty { src, dst, source, recover_source }
+    }
+}
+
 /// Copies a file while preserving all metadata:
 /// - Created and modified times
 /// - Extended attributes
 /// - 
-pub fn copy_preserved<P1: AsRef<Path>, P2: AsRef<Path>>(src: P1, dst: P2) -> Result<(), io::Error> {
-    fs::copy(&src, &dst)?;
-    
+pub fn copy_preserved<P1: AsRef<Path>, P2: AsRef<Path>>(src: P1, dst: P2, options: &CopyPreservedOptions) -> Result<(), CopyPreservedError> {
     let src = src.as_ref();
     let dst = dst.as_ref();
-    let metadata = src.metadata()?;
-    let src_cstr = CString::new(src.as_os_str().as_bytes())?;
-    let dst_cstr = CString::new(dst.as_os_str().as_bytes())?;
+    let metadata = src.metadata()
+        .map_err(|e| CopyPreservedError::clean(&src, &dst, e))?;
+    let src_cstr = CString::new(src.as_os_str().as_bytes())
+        .map_err(|e| CopyPreservedError::clean(&src, &dst, io::Error::new(io::ErrorKind::InvalidInput, e)))?;
+    let dst_cstr = CString::new(dst.as_os_str().as_bytes())
+        .map_err(|e| CopyPreservedError::clean(&src, &dst, io::Error::new(io::ErrorKind::InvalidInput, e)))?;
     
-    unsafe {
-        // ownership
-        libc::chown(
-            dst_cstr.as_ptr(),
-            metadata.uid(),
-            metadata.gid(),
-        );
-        
-        // permissons
-        libc::chmod(dst_cstr.as_ptr(), metadata.mode());
-        
-        let times = [
-            libc::timespec {
-                tv_sec: metadata.atime(),
-                tv_nsec: metadata.atime_nsec(),
-            },
-            libc::timespec {
-                tv_sec: metadata.mtime(),
-                tv_nsec: metadata.mtime_nsec(),
-            },
-        ];
-        
-        // created and modified times
-        libc::utimensat(
-            libc::AT_FDCWD,
-            dst_cstr.as_ptr(),
-            times.as_ptr(),
-            0,
-        );
-    }
+    fs::copy(&src, &dst)
+        .map_err(|e| recover(&src, &dst, e))?;
     
-    // extended attributes
-    copy_xattrs(&src_cstr, &dst_cstr)?;
+    chown(&dst_cstr, &metadata) // ownership
+        .map_err(|e| recover(&src, &dst, e))?;
+    
+    chmod(&dst_cstr, &metadata) // permissions
+        .map_err(|e| recover(&src, &dst, e))?;
+    
+    set_timestamps(&dst_cstr, &metadata) // created, modified 
+        .map_err(|e| recover(&src, &dst, e))?;
+    
+    copy_xattrs(&src_cstr, &dst_cstr, &options) // extended attributes
+        .map_err(|e| recover(src, dst, e))?;
     
     Ok(())
 }
 
-fn copy_xattrs(src: &CStr, dst: &CStr) -> io::Result<()> {
-    unsafe {
-        let size = libc::llistxattr(
+#[inline]
+fn err_io_other<T>(msg: &'static str) -> io::Result<T> {
+    io::Result::Err(io::Error::new(io::ErrorKind::Other, msg))
+}
+
+#[inline]
+fn io_data_error(msg: &'static str) -> io::Error {
+    io::Error::new(io::ErrorKind::InvalidData, msg)
+}
+
+fn chown(path: &CStr, meta: &fs::Metadata) -> io::Result<()> {
+    let code = unsafe {
+        libc::chown(
+            path.as_ptr(),
+            meta.uid(),
+            meta.gid(),
+        )
+    };
+    
+    //todo: match code
+        
+    Ok(())
+}
+
+fn chmod(path: &CStr, meta: &fs::Metadata) -> io::Result<()> {
+    let code = unsafe {
+        libc::chmod(path.as_ptr(), meta.mode())
+    };
+    
+    //todo: match
+    
+    Ok(())
+}
+
+fn set_timestamps(dst: &CStr, meta: &fs::Metadata) -> io::Result<()> {
+    let times = [
+        libc::timespec {
+            tv_sec: meta.atime(),
+            tv_nsec: meta.atime_nsec(),
+        },
+        libc::timespec {
+            tv_sec: meta.mtime(),
+            tv_nsec: meta.mtime_nsec(),
+        },
+    ];
+    
+    let code = unsafe {
+        libc::utimensat(
+            libc::AT_FDCWD,
+            dst.as_ptr(),
+            times.as_ptr(),
+            0,
+        )
+    };
+    
+    //todo: match code
+    
+    Ok(())
+}
+
+fn copy_xattrs(src: &CStr, dst: &CStr, options: &CopyPreservedOptions) -> io::Result<()> {
+    const UNSUPPORTED_OK: bool = true;
+    const E_LLISTXATTR_UNKNOWN: &'static str = "Unknown libc::llistxattr error";
+    const E_LGETXATTR_UNKNOWN: &'static str = "Unknown libc::lgetxattr error";
+    const E_LLISTXATTR_NAME: &'static str = "Invalid name from libc::llistxattr";
+    
+    fn err_llistxattr(unsupported_ok: bool) -> io::Result<()> {
+        let err = io::Error::last_os_error();
+        return match err.raw_os_error() {
+            Some(libc::ENOTSUP) if unsupported_ok => Ok(()),
+            Some(_) => Err(err),
+            None => err_io_other(E_LLISTXATTR_UNKNOWN),
+        }
+    }
+    
+    fn err_lgetxattr() -> io::Result<()> {
+        let err = io::Error::last_os_error();
+        return match err.raw_os_error() {
+            Some(_) => Err(err),
+            None => err_io_other(E_LLISTXATTR_UNKNOWN),
+        }
+    }
+
+    let code = unsafe {
+        libc::llistxattr(
             src.as_ptr(),
             ptr::null_mut(),
             0,
-        );
+        )
+    };
         
-        if size < 0 {
-            let err = io::Error::last_os_error();
-            return match err.raw_os_error().unwrap_or(i32::MIN) {
-                libc::ENOTSUP => Ok(()),
-                _ => Err(err),
-            };
-        } else if size < 1 {
-            return Ok(());
-        }
+    let size = match code {
+        -1 => return err_llistxattr(UNSUPPORTED_OK),
+        0 => return Ok(()),
+        n if n > 0 => n,
+        _ => return err_io_other(E_LLISTXATTR_UNKNOWN),
+    };
         
-        let mut list = vec![0u8; size as usize];
-        let code = libc::llistxattr(
+    let mut list = vec![0u8; size as usize];
+    let code = unsafe {
+        libc::llistxattr(
             src.as_ptr(),
             list.as_mut_ptr() as *mut i8,
             size as usize,
-        );
+        )
+    };
         
-        if code < 0 {
-            let err = io::Error::last_os_error();
-            return match err.raw_os_error().unwrap_or(i32::MIN) {
-                libc::ENOTSUP => Ok(()),
-                _ => Err(err),
-            };
-        } else if code < 1 {
-            return Ok(());
+    match code {
+        -1 => return err_llistxattr(!UNSUPPORTED_OK),
+        0 => return Ok(()),
+        n if n == size => (), // double-check
+        _ => return err_io_other(E_LLISTXATTR_UNKNOWN),
+    };
+        
+    let mut pos = 0;
+    let list_len = list.len();
+    while pos < list_len {
+        let name_start = pos;
+        while pos < list_len && list[pos] != 0 {
+            pos += 1;
         }
         
-        let mut pos = 0;
-        while pos < list.len() {
-            let name_start = pos;
-            while pos < list.len() && list[pos] != 0 {
-                pos += 1;
-            }
-            
-            if pos <= name_start {
-                break;
-            }
-            
-            let name = CStr::from_bytes_with_nul(&list[name_start..=pos])
-                .map_err(|_| io::Error::new(io::ErrorKind::InvalidData, "Invalid xattr name"))?;
-            
-            let value_size = libc::lgetxattr(
+        if pos <= name_start {
+            break;
+        }
+        
+        let name = CStr::from_bytes_with_nul(&list[name_start..=pos])
+            .map_err(|_| io_data_error(E_LLISTXATTR_NAME))?;
+        
+        let code = unsafe {
+            libc::lgetxattr(
                 src.as_ptr(),
                 name.as_ptr(),
                 ptr::null_mut(),
                 0,
-            );
-            
-            if value_size > 0 {
-                let mut value = vec![0u8; value_size as usize];
+            )
+        };
+        
+        let value_size = match code {
+            -1 => return err_lgetxattr(),
+            0 => return Ok(()),
+            n if n > 0 => n,
+            _ => return err_io_other(E_LGETXATTR_UNKNOWN),
+        };
+        
+        if value_size > 0 {
+            let mut value = vec![0u8; value_size as usize];
+            let code = unsafe {
                 libc::getxattr(
                     src.as_ptr(),
                     name.as_ptr(),
                     value.as_mut_ptr() as *mut libc::c_void,
                     value_size as usize,
-                );
-                
+                )
+            };
+            
+            //todo: match code
+            
+            let code = unsafe {
                 libc::setxattr(
                     dst.as_ptr(),
                     name.as_ptr(),
                     value.as_ptr() as *const libc::c_void,
                     value.len(),
                     0,
-                );
-            }
+                )
+            };
             
-            pos += 1;
+            // todo: match code
         }
+        
+        pos += 1;
     }
     
     Ok(())
