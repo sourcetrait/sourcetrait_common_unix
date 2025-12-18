f3f5196f16cd552f35e27823d7172b6809be1ae5
roylaurie <roy.laurie.pub@sourcetrait.com>
2025-12-18 10:41:32 -0800

    unix copy_preserved ready for testing

--- crates/sourcetrait_crossplat_unix/src/unix_fs/copy_preserved.rs
+++ crates/sourcetrait_crossplat_unix/src/unix_fs/copy_preserved.rs
@@ -1,8 +1,53 @@
 use crate::*;
 
+/// Options used with [copy_preserved].
 #[derive(Debug, Clone, Default, PartialEq, Eq, Hash)]
 pub struct CopyPreservedOptions {
-    pub lossy_attributes: bool,
+    /// Ignore when extended attributes exist for the source path,
+    /// but are not supported by destination filesystem.
+    /// - FALSE (default): Operation fails and throws an error  
+    /// - TRUE: Operation proceeds
+    pub lossy_extended_attributes: bool,
+}
+
+/// Copies a file while preserving all extended metadata:
+/// - Ownership
+/// - Permissions
+/// - Created and modified timestamps
+/// - Extended attributes
+/// 
+/// Operates directly on symbolic links (does not follow).
+/// 
+/// Note: `chmod` is *usually* ignored for symbolic links. Outliers:
+/// - Various BSD variants, including MacOS
+/// - Solaris variants
+/// - AIX variants 
+pub fn copy_preserved<P1: AsRef<Path>, P2: AsRef<Path>>(src: P1, dst: P2, options: &CopyPreservedOptions) -> Result<(), CopyPreservedError> {
+    let src = src.as_ref();
+    let dst = dst.as_ref();
+    let metadata = src.metadata()
+        .map_err(|e| CopyPreservedError::clean(&src, &dst, e))?;
+    let src_cstr = CString::new(src.as_os_str().as_bytes())
+        .map_err(|e| CopyPreservedError::clean(&src, &dst, io::Error::new(io::ErrorKind::InvalidInput, e)))?;
+    let dst_cstr = CString::new(dst.as_os_str().as_bytes())
+        .map_err(|e| CopyPreservedError::clean(&src, &dst, io::Error::new(io::ErrorKind::InvalidInput, e)))?;
+    
+    fs::copy(&src, &dst)
+        .map_err(|e| recover(&src, &dst, e))?;
+    
+    chown(&dst_cstr, &metadata) // ownership
+        .map_err(|e| recover(&src, &dst, e))?;
+    
+    chmod(&dst_cstr, &metadata) // permissions
+        .map_err(|e| recover(&src, &dst, e))?;
+    
+    set_timestamps(&dst_cstr, &metadata) // created, modified 
+        .map_err(|e| recover(&src, &dst, e))?;
+    
+    copy_xattrs(&src_cstr, &dst_cstr, &options) // extended attributes
+        .map_err(|e| recover(src, dst, e))?;
+    
+    Ok(())
 }
 
 /// Describes an IO error that was either cleanly recovered from or not.
@@ -43,38 +88,6 @@ fn recover<P1: AsRef<Path>, P2: AsRef<Path>>(src: P1, dst: P2, source: io::Error
     }
 }
 
-/// Copies a file while preserving all metadata:
-/// - Created and modified times
-/// - Extended attributes
-/// - 
-pub fn copy_preserved<P1: AsRef<Path>, P2: AsRef<Path>>(src: P1, dst: P2, options: &CopyPreservedOptions) -> Result<(), CopyPreservedError> {
-    let src = src.as_ref();
-    let dst = dst.as_ref();
-    let metadata = src.metadata()
-        .map_err(|e| CopyPreservedError::clean(&src, &dst, e))?;
-    let src_cstr = CString::new(src.as_os_str().as_bytes())
-        .map_err(|e| CopyPreservedError::clean(&src, &dst, io::Error::new(io::ErrorKind::InvalidInput, e)))?;
-    let dst_cstr = CString::new(dst.as_os_str().as_bytes())
-        .map_err(|e| CopyPreservedError::clean(&src, &dst, io::Error::new(io::ErrorKind::InvalidInput, e)))?;
-    
-    fs::copy(&src, &dst)
-        .map_err(|e| recover(&src, &dst, e))?;
-    
-    chown(&dst_cstr, &metadata) // ownership
-        .map_err(|e| recover(&src, &dst, e))?;
-    
-    chmod(&dst_cstr, &metadata) // permissions
-        .map_err(|e| recover(&src, &dst, e))?;
-    
-    set_timestamps(&dst_cstr, &metadata) // created, modified 
-        .map_err(|e| recover(&src, &dst, e))?;
-    
-    copy_xattrs(&src_cstr, &dst_cstr, &options) // extended attributes
-        .map_err(|e| recover(src, dst, e))?;
-    
-    Ok(())
-}
-
 #[inline]
 fn err_io_other<T>(msg: &'static str) -> io::Result<T> {
     io::Result::Err(io::Error::new(io::ErrorKind::Other, msg))
@@ -86,30 +99,70 @@ fn io_data_error(msg: &'static str) -> io::Error {
 }
 
 fn chown(path: &CStr, meta: &fs::Metadata) -> io::Result<()> {
+    const E_LCHOWN_UNKNOWN: &'static str = "Unknown libc::lchown error";
+    
+    fn err_lchown() -> io::Result<()> {
+        let err = io::Error::last_os_error();
+        return match err.raw_os_error() {
+            Some(_) => Err(err),
+            None => err_io_other(E_LCHOWN_UNKNOWN),
+        }
+    }
+    
     let code = unsafe {
-        libc::chown(
+        libc::lchown(
             path.as_ptr(),
             meta.uid(),
             meta.gid(),
         )
     };
     
-    //todo: match code
-        
-    Ok(())
+    match code {
+        0 => Ok(()),
+        -1 => err_lchown(),
+        _ => err_io_other(E_LCHOWN_UNKNOWN),
+    }
 }
 
 fn chmod(path: &CStr, meta: &fs::Metadata) -> io::Result<()> {
+    const E_FCHMODAT_UNKNOWN: &'static str = "Unknown libc::fchmodat error";
+    
+    fn check_fchmodat() -> io::Result<()> {
+        let err = io::Error::last_os_error();
+        return match err.raw_os_error() {
+            Some(libc::ENOTSUP) => Ok(()),
+            Some(_) => Err(err),
+            None => err_io_other(E_FCHMODAT_UNKNOWN),
+        }
+    }
+    
     let code = unsafe {
-        libc::chmod(path.as_ptr(), meta.mode())
+        libc::fchmodat(
+            libc::AT_FDCWD,
+            path.as_ptr(),
+            meta.mode(),
+            libc::AT_SYMLINK_NOFOLLOW,
+        )
     };
     
-    //todo: match
-    
-    Ok(())
+    match code {
+        0 => Ok(()),
+        -1 => check_fchmodat(),
+        _ => err_io_other(E_FCHMODAT_UNKNOWN),
+    }
 }
 
 fn set_timestamps(dst: &CStr, meta: &fs::Metadata) -> io::Result<()> {
+    const E_UTIMENSAT_UNKNOWN: &'static str = "Unknown libc::utimensat error";
+    
+    fn err_utimensat() -> io::Result<()> {
+        let err = io::Error::last_os_error();
+        return match err.raw_os_error() {
+            Some(_) => Err(err),
+            None => err_io_other(E_UTIMENSAT_UNKNOWN),
+        }
+    }
+    
     let times = [
         libc::timespec {
             tv_sec: meta.atime(),
@@ -126,19 +179,22 @@ fn set_timestamps(dst: &CStr, meta: &fs::Metadata) -> io::Result<()> {
             libc::AT_FDCWD,
             dst.as_ptr(),
             times.as_ptr(),
-            0,
+            libc::AT_SYMLINK_NOFOLLOW,
         )
     };
     
-    //todo: match code
-    
-    Ok(())
+    match code {
+        0 => Ok(()),
+        -1 => err_utimensat(),
+        _ => err_io_other(E_UTIMENSAT_UNKNOWN),
+    }
 }
 
 fn copy_xattrs(src: &CStr, dst: &CStr, options: &CopyPreservedOptions) -> io::Result<()> {
     const UNSUPPORTED_OK: bool = true;
     const E_LLISTXATTR_UNKNOWN: &'static str = "Unknown libc::llistxattr error";
     const E_LGETXATTR_UNKNOWN: &'static str = "Unknown libc::lgetxattr error";
+    const E_LSETXATTR_UNKNOWN: &'static str = "Unknown libc::lsetxattr error";
     const E_LLISTXATTR_NAME: &'static str = "Invalid name from libc::llistxattr";
     
     fn err_llistxattr(unsupported_ok: bool) -> io::Result<()> {
@@ -154,7 +210,16 @@ fn copy_xattrs(src: &CStr, dst: &CStr, options: &CopyPreservedOptions) -> io::Re
         let err = io::Error::last_os_error();
         return match err.raw_os_error() {
             Some(_) => Err(err),
-            None => err_io_other(E_LLISTXATTR_UNKNOWN),
+            None => err_io_other(E_LGETXATTR_UNKNOWN),
+        }
+    }
+    
+    fn check_lsetxattr(unsupported_ok: bool) -> io::Result<()> {
+        let err = io::Error::last_os_error();
+        return match err.raw_os_error() {
+            Some(libc::ENOTSUP) if unsupported_ok => Ok(()),
+            Some(_) => Err(err),
+            None => err_io_other(E_LSETXATTR_UNKNOWN),
         }
     }
 
@@ -189,6 +254,7 @@ fn copy_xattrs(src: &CStr, dst: &CStr, options: &CopyPreservedOptions) -> io::Re
         _ => return err_io_other(E_LLISTXATTR_UNKNOWN),
     };
         
+    // each name is null-terminated
     let mut pos = 0;
     let list_len = list.len();
     while pos < list_len {
@@ -215,36 +281,41 @@ fn copy_xattrs(src: &CStr, dst: &CStr, options: &CopyPreservedOptions) -> io::Re
         
         let value_size = match code {
             -1 => return err_lgetxattr(),
-            0 => return Ok(()),
             n if n > 0 => n,
             _ => return err_io_other(E_LGETXATTR_UNKNOWN),
         };
         
-        if value_size > 0 {
-            let mut value = vec![0u8; value_size as usize];
-            let code = unsafe {
-                libc::getxattr(
-                    src.as_ptr(),
-                    name.as_ptr(),
-                    value.as_mut_ptr() as *mut libc::c_void,
-                    value_size as usize,
-                )
-            };
-            
-            //todo: match code
-            
-            let code = unsafe {
-                libc::setxattr(
-                    dst.as_ptr(),
-                    name.as_ptr(),
-                    value.as_ptr() as *const libc::c_void,
-                    value.len(),
-                    0,
-                )
-            };
-            
-            // todo: match code
-        }
+        let mut value = vec![0u8; value_size as usize];
+        let code = unsafe {
+            libc::lgetxattr(
+                src.as_ptr(),
+                name.as_ptr(),
+                value.as_mut_ptr() as *mut libc::c_void,
+                value_size as usize,
+            )
+        };
+        
+        match code {
+            -1 => return err_lgetxattr(),
+            n if n == value_size => (), // double-check
+            _ => return err_io_other(E_LGETXATTR_UNKNOWN),
+        };
+        
+        let code = unsafe {
+            libc::lsetxattr(
+                dst.as_ptr(),
+                name.as_ptr(),
+                value.as_ptr() as *const libc::c_void,
+                value.len(),
+                0,
+            )
+        };
+        
+        match code {
+            0 => (),
+            -1 => check_lsetxattr(options.lossy_extended_attributes)?,
+            _ => return err_io_other(E_LSETXATTR_UNKNOWN),
+        };
         
         pos += 1;
     }
