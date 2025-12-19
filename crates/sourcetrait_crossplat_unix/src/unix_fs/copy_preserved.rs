3a6e4e25c01e3bc0d67d9fe743636de4a7169adb
roylaurie <roy.laurie.pub@sourcetrait.com>
2025-12-19 06:47:22 -0800

    wip copy nofollow

--- crates/sourcetrait_crossplat_unix/src/unix_fs/copy_preserved.rs
+++ crates/sourcetrait_crossplat_unix/src/unix_fs/copy_preserved.rs
@@ -1,13 +1,33 @@
 use crate::*;
 
 /// Options used with [copy_preserved].
-#[derive(Debug, Clone, Default, PartialEq, Eq, Hash)]
-pub struct CopyPreservedOptions {
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct CopyOptions {
+    /// Dereference symbolic links.
+    /// - FALSE: Operates on the symbolic link itself
+    /// - TRUE (default): Operates on the actual file linked to
+    pub follow_symlinks: bool,
+    
     /// Ignore when extended attributes exist for the source path,
     /// but are not supported by destination filesystem.
     /// - FALSE (default): Operation fails and throws an error  
     /// - TRUE: Operation proceeds
     pub lossy_extended_attributes: bool,
+}
+
+impl CopyOptions {
+    pub const DEFAULT: Self = Self {
+        follow_symlinks: true,
+        lossy_extended_attributes: false,
+    };
+    
+    pub const FOLLOW_SYMLINKS: bool = true;
+    pub const LOSSY_EXT_ATTRIBUTES: bool = false;
+}
+
+impl Default for CopyOptions {
+    #[inline]
+    fn default() -> Self { Self::DEFAULT }
 }
 
 /// Copies a file while preserving all extended metadata:
@@ -22,29 +42,29 @@ pub struct CopyPreservedOptions {
 /// - Various BSD variants, including MacOS
 /// - Solaris variants
 /// - AIX variants 
-pub fn copy_preserved<P1: AsRef<Path>, P2: AsRef<Path>>(src: P1, dst: P2, options: &CopyPreservedOptions) -> Result<(), CopyPreservedError> {
+pub fn copy_preserved<P1: AsRef<Path>, P2: AsRef<Path>>(src: P1, dst: P2, opts: &CopyOptions) -> Result<(), CopyError> {
     let src = src.as_ref();
     let dst = dst.as_ref();
     let metadata = src.metadata()
-        .map_err(|e| CopyPreservedError::clean(&src, &dst, e))?;
+        .map_err(|e| CopyError::clean(&src, &dst, e))?;
     let src_cstr = CString::new(src.as_os_str().as_bytes())
-        .map_err(|e| CopyPreservedError::clean(&src, &dst, io::Error::new(io::ErrorKind::InvalidInput, e)))?;
+        .map_err(|e| CopyError::clean(&src, &dst, io::Error::new(io::ErrorKind::InvalidInput, e)))?;
     let dst_cstr = CString::new(dst.as_os_str().as_bytes())
-        .map_err(|e| CopyPreservedError::clean(&src, &dst, io::Error::new(io::ErrorKind::InvalidInput, e)))?;
+        .map_err(|e| CopyError::clean(&src, &dst, io::Error::new(io::ErrorKind::InvalidInput, e)))?;
     
     fs::copy(&src, &dst)
         .map_err(|e| recover(&src, &dst, e))?;
     
-    chown(&dst_cstr, &metadata) // ownership
+    chown(&dst_cstr, &metadata, &opts) // ownership
         .map_err(|e| recover(&src, &dst, e))?;
     
-    chmod(&dst_cstr, &metadata) // permissions
+    chmod(&dst_cstr, &metadata, &opts) // permissions
         .map_err(|e| recover(&src, &dst, e))?;
     
     set_timestamps(&dst_cstr, &metadata) // created, modified 
         .map_err(|e| recover(&src, &dst, e))?;
     
-    copy_xattrs(&src_cstr, &dst_cstr, &options) // extended attributes
+    copy_xattrs(&src_cstr, &dst_cstr, &opts) // extended attributes
         .map_err(|e| recover(src, dst, e))?;
     
     Ok(())
@@ -54,7 +74,7 @@ pub fn copy_preserved<P1: AsRef<Path>, P2: AsRef<Path>>(src: P1, dst: P2, option
 /// A clean recovery removed any file that was created.
 /// A dirty recovery failed to remove any file that was created. 
 #[derive(Debug, snafu::Snafu)]
-pub enum CopyPreservedError {
+pub enum CopyError {
     Clean {
         src: PathBuf,
         dst: PathBuf,
@@ -68,23 +88,23 @@ pub enum CopyPreservedError {
     }
 }
 
-impl CopyPreservedError {
+impl CopyError {
     fn clean<P1: AsRef<Path>, P2: AsRef<Path>>(src: P1, dst: P2, source: io::Error) -> Self {
         Self::Clean { src: src.as_ref().into(), dst: dst.as_ref().into(), source }
     }
 }
 
-fn recover<P1: AsRef<Path>, P2: AsRef<Path>>(src: P1, dst: P2, source: io::Error) -> CopyPreservedError {
+fn recover<P1: AsRef<Path>, P2: AsRef<Path>>(src: P1, dst: P2, source: io::Error) -> CopyError {
     let src = src.as_ref().to_path_buf();
     let dst = dst.as_ref().to_path_buf();
     
     if fs::exists(&dst).is_ok_and(|exists| !exists) {
-        return CopyPreservedError::Clean { src, dst, source };
+        return CopyError::Clean { src, dst, source };
     }
     
     match fs::remove_file(&dst) {
-        Ok(_) => CopyPreservedError::Clean { src, dst, source },
-        Err(recover_source) => CopyPreservedError::Dirty { src, dst, source, recover_source }
+        Ok(_) => CopyError::Clean { src, dst, source },
+        Err(recover_source) => CopyError::Dirty { src, dst, source, recover_source }
     }
 }
 
@@ -98,33 +118,51 @@ fn io_data_error(msg: &'static str) -> io::Error {
     io::Error::new(io::ErrorKind::InvalidData, msg)
 }
 
-fn chown(path: &CStr, meta: &fs::Metadata) -> io::Result<()> {
-    const E_LCHOWN_UNKNOWN: &'static str = "Unknown libc::lchown error";
+/// [man page](https://man7.org/linux/man-pages/man2/lchown.2.html)
+fn chown(path: &CStr, meta: &fs::Metadata, opts: &CopyOptions) -> io::Result<()> {
+    #[inline]
+    const fn e_chown_unknown(opts: &CopyOptions) -> &'static str {
+        const E_CHOWN_UNKNOWN: &'static str = "Unknown libc::chown error";
+        const E_LCHOWN_UNKNOWN: &'static str = "Unknown libc::lchown error";
+        
+        match opts.follow_symlinks {
+            true => E_CHOWN_UNKNOWN,
+            false => E_LCHOWN_UNKNOWN,
+        }
+    }
     
-    fn err_lchown() -> io::Result<()> {
+    fn err_chown(opts: &CopyOptions) -> io::Result<()> {
         let err = io::Error::last_os_error();
         return match err.raw_os_error() {
             Some(_) => Err(err),
-            None => err_io_other(E_LCHOWN_UNKNOWN),
+            None => err_io_other(e_chown_unknown(&opts)),
         }
     }
     
     let code = unsafe {
-        libc::lchown(
-            path.as_ptr(),
-            meta.uid(),
-            meta.gid(),
-        )
+        match opts.follow_symlinks {
+            true => libc::chown(
+                path.as_ptr(),
+                meta.uid(),
+                meta.gid(),
+            ),
+            false => libc::lchown(
+                path.as_ptr(),
+                meta.uid(),
+                meta.gid(),
+            ),
+        }
     };
     
     match code {
         0 => Ok(()),
-        -1 => err_lchown(),
-        _ => err_io_other(E_LCHOWN_UNKNOWN),
+        -1 => err_chown(&opts),
+        _ => err_io_other(e_chown_unknown(&opts)),
     }
 }
 
-fn chmod(path: &CStr, meta: &fs::Metadata) -> io::Result<()> {
+/// [man page](https://man7.org/linux/man-pages/man2/chmod.2.html)
+fn chmod(path: &CStr, meta: &fs::Metadata, opts: &CopyOptions) -> io::Result<()> {
     const E_FCHMODAT_UNKNOWN: &'static str = "Unknown libc::fchmodat error";
     
     fn check_fchmodat() -> io::Result<()> {
@@ -136,12 +174,17 @@ fn chmod(path: &CStr, meta: &fs::Metadata) -> io::Result<()> {
         }
     }
     
+    let flags = match opts.follow_symlinks {
+        true => 0,
+        false => libc::AT_SYMLINK_NOFOLLOW,
+    };
+    
     let code = unsafe {
         libc::fchmodat(
             libc::AT_FDCWD,
             path.as_ptr(),
             meta.mode(),
-            libc::AT_SYMLINK_NOFOLLOW,
+            flags,
         )
     };
     
@@ -152,7 +195,8 @@ fn chmod(path: &CStr, meta: &fs::Metadata) -> io::Result<()> {
     }
 }
 
-fn set_timestamps(dst: &CStr, meta: &fs::Metadata) -> io::Result<()> {
+/// [man page](https://man7.org/linux/man-pages/man2/utimensat.2.html)
+fn set_timestamps(dst: &CStr, meta: &fs::Metadata, opts: &CopyOptions) -> io::Result<()> {
     const E_UTIMENSAT_UNKNOWN: &'static str = "Unknown libc::utimensat error";
     
     fn err_utimensat() -> io::Result<()> {
@@ -174,12 +218,17 @@ fn set_timestamps(dst: &CStr, meta: &fs::Metadata) -> io::Result<()> {
         },
     ];
     
+    let flags = match opts.follow_symlinks {
+        true => 0,
+        false => libc::AT_SYMLINK_NOFOLLOW,
+    };
+    
     let code = unsafe {
         libc::utimensat(
             libc::AT_FDCWD,
             dst.as_ptr(),
             times.as_ptr(),
-            libc::AT_SYMLINK_NOFOLLOW,
+            flags,
         )
     };
     
@@ -190,12 +239,16 @@ fn set_timestamps(dst: &CStr, meta: &fs::Metadata) -> io::Result<()> {
     }
 }
 
-fn copy_xattrs(src: &CStr, dst: &CStr, options: &CopyPreservedOptions) -> io::Result<()> {
+fn copy_xattrs(src: &CStr, dst: &CStr, options: &CopyOptions) -> io::Result<()> {
     const UNSUPPORTED_OK: bool = true;
     const E_LLISTXATTR_UNKNOWN: &'static str = "Unknown libc::llistxattr error";
+    const E_LISTXATTR_UNKNOWN: &'static str = "Unknown libc::listxattr error";
     const E_LGETXATTR_UNKNOWN: &'static str = "Unknown libc::lgetxattr error";
+    const E_GETXATTR_UNKNOWN: &'static str = "Unknown libc::getxattr error";
     const E_LSETXATTR_UNKNOWN: &'static str = "Unknown libc::lsetxattr error";
+    const E_SETXATTR_UNKNOWN: &'static str = "Unknown libc::setxattr error";
     const E_LLISTXATTR_NAME: &'static str = "Invalid name from libc::llistxattr";
+    const E_LISTXATTR_NAME: &'static str = "Invalid name from libc::listxattr";
     
     fn err_llistxattr(unsupported_ok: bool) -> io::Result<()> {
         let err = io::Error::last_os_error();
